d6840f34bd1c84b6a30158bfe71df2f947146f2d
Md Oadud Hossain <oadudhossain171@gmail.com>
2026-09-17 02:10:26 +0600

    remoed results

--- data/education.js
+++ data/education.js
@@ -8,7 +8,7 @@ export const education = [
     qualification: "BSc in Computer Science and Engineering",
     institution: "Daffodil International University",
     period: "2017-2021",
-    result: "3.66",
+    result: "",
     details:
       "Focused on web development, programming, databases, web technologies, and software engineering principles through academic and practical projects.",
   },
@@ -18,7 +18,7 @@ export const education = [
     qualification: "HSC - Science",
     institution: "Jahangirpur Govt. College Mohadevpur",
     period: "2015/16",
-    result: "3.83",
+    result: "",
     details:
       "Completed higher secondary education with a focus on academic fundamentals, discipline, and preparation for university studies.",
   },
@@ -28,7 +28,7 @@ export const education = [
     qualification: "SSC - Science",
     institution: "Mohadevpur S.M High School",
     period: "2009-2014",
-    result: "4.69",
+    result: "",
     details:
       "Completed secondary education with a strong foundation in core academic subjects and essential learning skills.",
   },
